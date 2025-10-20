abd76a6eab9e0b5a24b44ba0f0e55ba11a51ab64
grant <grant5014792@gmail.com>
2025-10-20 17:36:47 +0800

    modify syntax style second

--- Calc.py
+++ Calc.py
@@ -2,14 +2,11 @@ class Calculator:
     def add(self, a, b):
         return a + b
 
-
     def sub(self, a, b):
         return a - b
 
-
     def mul(self, a, b):
         return a * b
-
 
     def div(self, a, b):
         return a / b
